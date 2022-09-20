0b2e7d975419b81ebec30362666a369a32a091e3
Ayilara Busari Dare <108156117+Ayilara1@users.noreply.github.com>
2022-09-20 19:30:02 +0100

    Create lists.h

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,43 @@
+
+aniekeme112
+/
+alx-higher_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+alx-higher_level_programming/0x01-python-if_else_loops_functions/lists.h
+@aniekeme112
+aniekeme112 Add files via upload
+ 1 contributor
+26 lines (21 sloc)  496 Bytes
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTS_H */
